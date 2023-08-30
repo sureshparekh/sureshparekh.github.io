0f8262466656f8d9b0174fbe6e5dab4246aed26c
Suresh Parekh <sureshparekh8412@gmail.com>
2023-08-30 17:35:39 +0530

    Add files via upload

---
+++ projectilemotion/images/js/js2.js
@@ -0,0 +1,55 @@
+var text01 = "Reset";                    
+var text02 = ["Start", "Pause", "Resume"];          
+var text03 = "Slow motion";
+var text04 = "Initial height:";
+var text05 = "Initial speed:";
+var text06 = "Angle:";
+var text07 = "Mass:"; 
+var text08 = "Gravitational acceleration:";
+var text09 = "Position";
+var text10 = "Velocity";
+var text11 = "Acceleration";
+var text12 = "Force";
+var text13 = "Energy";
+
+var author = "";
+
+// Symbols  
+
+var decimalSeparator = ".";                                
+var meter = "m";                                           
+var meterPerSecond = "m/s";                                
+var meterPerSecond2 = "m/s&sup2;";                         
+var kilogram = "kg";                                       
+var degree = "&deg;";                                      
+
+var text14 = "(in m)";                                     
+var text15 = "Position:";
+var text16 = "(horizontal)";
+var text17 = "(vertical)";
+var text18 = "Horizontal distance:";
+var text19 = "Maximum height:";
+var text20 = "Time:";
+var text21 = "Components of velocity:";
+var text22 = "Magnitude of velocity:";
+var text23 = "Angle:";
+var text24 = "Acceleration:";
+var text25 = "Force:";
+var text26 = "Kinetic energy:";
+var text27 = "Potential energy:";
+var text28 = "Total energy:";
+
+
+
+var symbolX = "x";                                         
+var symbolY = "y";                                         
+var symbolVelocity = "v";                                  
+var meterUnicode = "m";                                    
+var secondUnicode = "s";                                   
+var meterPerSecondUnicode = "m/s";                         
+var meterPerSecond2Unicode = "m/s\u00b2";                  
+var newtonUnicode = "N";                                   
+var jouleUnicode = "J";                                    
+var degreeUnicode = "\u00b0";                              
+
+
